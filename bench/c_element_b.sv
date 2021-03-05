0b0979339e1815d544e207a4942b8dc404bcf4c7
FloorCows <merio.cristiano@grenoble-inp.org>
2021-03-05 10:56:39 +0100

    work

--- bench/c_element_b.sv
+++ bench/c_element_b.sv
@@ -4,33 +4,47 @@
 */
 
 
-timeunit 1ns;
-timeprecision 1ns;
 
 module c_element_b ();
+
+  timeunit 1ns;
+  timeprecision 100ps;
 
   logic reset;
   logic in1, in2;
   logic out;
+  logic clk;
 
   c_element_m c_element (
     .rst(reset),
     .o(out),
-    .a(in1),
+    .a(clk),
     .b(in2)
   );
 
-  initial $timeformat ( -9, 1, "ns", 12 );
+  localparam clk_period = 10;
+  always #(clk_period/2) clk = ~clk;
 
   initial begin
+    $timeformat ( -9, 1, " ns", 9 );
+  end
+
+  initial begin
+    clk = 1'b0;
     in1 = 1'b1;
     in2 = 1'b1;
-    #30;
+    #(10);
     in1 = 1'b0;
     in2 = 1'b1;
-    #30;
+    #(10);
     in1 = 1'b0;
     in2 = 1'b0;
+    #(10);
+    in1 = 1'b1;
+    in2 = 1'b0;
+    #(10);
+    in1 = 1'b0;
+    in2 = 1'b1;
   end
 
 endmodule
